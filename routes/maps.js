6bcd75181300c8fb7dc63f7c08528d9251dbaca6
Manuel Casanova <manucasanova@hotmail.com>
2022-04-27 13:35:56 -0700

    testing pg

--- routes/maps.js
+++ routes/maps.js
@@ -2,6 +2,12 @@ const express = require('express');
 // router is a smaller module from express
 const router = express.Router();
 
+const { Pool } = require("pg");
+const dbParams = require("../lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+
 // TEMPLATE FOR ROUTES
 // module.exports = (db) => {
 //   router.get("/", (req, res) => {
@@ -14,6 +20,18 @@ module.exports = (db) => {//rendering a newmap page
   router.get("/new", (req, res) => {//app.use("/maps", mapRoutes(db)); from server file is a base, then we add /new
       res.render("new");
   });
+
+  router.post('/', (req, res) => {
+    const addMap = function(map) {
+    const queryParams = [
+      map['title'],
+      map['description'],
+    ];
+
+  const queryString = `INSERT INTO maps (title, description) VALUES ($1, $2) RETURNING *;`; return pool.query(queryString, queryParams).then((res) => res.rows);
+}
+})
+
 
   // INCOMPLETE QUERY TEMPLATE
   // router.post("/new", (req, res) => {
