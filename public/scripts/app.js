0b889c02c69cb9df14ab91251a606e869060779a
Liam Smith <liam.s.smith@gmail.com>
2022-04-28 19:31:09 +0000

    create jquery function for dynamic html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+
+const showAllMaps = function(mapsData) {
+const $maps = $(
+  `<tbody>
+      <tr>
+        <td><img src=${nonsense/* MapTable*/}></td>
+        <td>${nonsense/*MapTitle*/}</td>
+      </tr>
+  </tbody>
+  `
+  );
+  return $maps
+};
