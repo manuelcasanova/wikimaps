007a7df15d922e122eaf1323db56b60b967c458b
Manuel Casanova <manucasanova@hotmail.com>
2022-04-29 11:57:35 -0700

    add fake data

---
+++ db/seeds/03_points.sql
@@ -0,0 +1,2 @@
+-- Widgets table seeds here (Example)
+INSERT INTO points (title, description, image, latitude, longitude) VALUES ('CN Tower', 'The highest tower', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Toronto_-_ON_-_Toronto_Harbourfront7.jpg/1200px-Toronto_-_ON_-_Toronto_Harbourfront7.jpg', 43.64249365536619, -79.38716085857133);
