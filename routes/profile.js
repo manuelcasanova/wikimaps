02f3ad09fc82c2b2352f4d84327de15068287cb7
Liam Smith <liam.s.smith@gmail.com>
2022-05-03 15:53:56 +0000

    delete redundant file

--- routes/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-
-module.exports = (db) => {//rendering points page
-  router.get("/profile", (req, res) => {
-      res.render("profile");
-  })
-  return router;
-};
