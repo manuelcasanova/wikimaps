8c7640a56a9c702a9f6adcea12ae94728032d4cb
Liam Smith <liam.s.smith@gmail.com>
2022-05-06 02:23:50 +0000

    render profile page if user logged in, set up mock profile page

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,18 @@ const express = require('express');
 const router  = express.Router();
 
 
+// const userData = function(db, userid) {
+//   const queryString = `SELECT users.*,
+//   maps.id AS map_id,
+//   maps.title AS map_title,
+//   maps.description AS map_description
+//   FROM users
+//   LEFT JOIN maps ON users.id = map.created_by
+//   WHERE users.id = $1;`;
+//   return db.query(queryString)
+//   .then((res) => res.rows);
+// };
+
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -38,6 +50,28 @@ module.exports = (db) => {
   router.post("/logout", (req, res) => {
     req.session = null; //To destroy a session instead of `cookie parser res.clearCookie("userID");`
     res.redirect('/');
+  });
+
+  router.get("/profile/:id", (req, res) => {
+    const signedInUser = req.params.id;
+    db.query(`SELECT users.*,
+    maps.id AS map_id,
+    maps.title AS map_title,
+    maps.description AS map_description
+    FROM users
+    LEFT JOIN maps ON users.id = maps.created_by
+    WHERE users.id = $1;`, [signedInUser]
+    )
+      .then((data) => {
+        const userData = data.rows;
+
+        const userid = req.session.userid;
+        res.render("profile", { userData, userid, signedInUser});
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+      });
   });
 
   return router;
