093ad74979f2bfe4ce7898a4b5ced7f1513ffcec
Manuel Casanova <manucasanova@hotmail.com>
2022-05-06 13:58:13 -0700

    Add hard coded pins and modify maps descriptions

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,6 +1,6 @@
 -- Widgets table seeds here (Example)
-INSERT INTO maps (title, description, created_by) VALUES ('Halifax touristic points', 'The best places for tourists', 1);
+INSERT INTO maps (title, description, created_by) VALUES ('Halifax touristic points', 'Cool touristic spots', 1);
 
-INSERT INTO maps (title, description, created_by) VALUES ('Toronto touristic points', 'The best places for tourists in Toronto', 1);
+INSERT INTO maps (title, description, created_by) VALUES ('Toronto touristic points', 'The best places for tourists', 1);
 
-INSERT INTO maps (title, description, created_by) VALUES ('Vancouver touristic points', 'The best places for tourists in Vancouver', 2);
+INSERT INTO maps (title, description, created_by) VALUES ('Vancouver touristic points', 'Nice spots in the city', 2);
--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -2,9 +2,27 @@ INSERT INTO points (title, description, image, created_by, map_id, created_at, l
 VALUES('Peggys Cove', 'A cove named for Peggy', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/72/57/9e/caption.jpg?w=600&h=600&s=1', 1, 1, '2018-08-27T07:00:00.000Z', 44.4939, -63.919132);
 
 INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
-VALUES('CN Tower', 'Needle in the Sky', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/CN_Tower_-_Toronto%2C_Ontario%2C_Canada_-_August_10%2C_2015_05.jpg/800px-CN_Tower_-_Toronto%2C_Ontario%2C_Canada_-_August_10%2C_2015_05.jpg?20160906125433', 2, 2, '2018-08-27T07:00:00.000Z', 43.642567, -79.387054);
+VALUES('Fort Charlotte', 'Georges Island National Historic Site', 'https://hmhps.ca/images/locations/georges-island-fort-charlotte/georges-island-fort-charlotte-1.jpg', 2, 1, '2018-08-27T07:00:00.000Z', 44.64081123548862, -63.5592031453874);
 
+INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
+VALUES('Macdonald Bridge', 'Suspension bridge', 'https://live.staticflickr.com/65535/49427030841_399df36d89_b.jpg', 1, 1, '2018-08-27T07:00:00.000Z', 44.6638794280814, -63.58470852772923);
+
+INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
+VALUES('CN Tower', 'Needle in the Sky', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/CN_Tower_-_Toronto%2C_Ontario%2C_Canada_-_August_10%2C_2015_05.jpg/800px-CN_Tower_-_Toronto%2C_Ontario%2C_Canada_-_August_10%2C_2015_05.jpg?20160906125433', 3, 2, '2018-08-27T07:00:00.000Z', 43.642567, -79.387054);
+
+INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
+VALUES('Rogers center', 'Baseball stadium', 'https://globalnews.ca/wp-content/uploads/2013/04/global-news-stock-jeremy-cohn-rogerscentre.jpg?quality=85&strip=all', 3, 2, '2018-08-27T07:00:00.000Z', 43.64155537421714, -79.38920695789298);
+
+INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
+VALUES('Cherry beach', 'Recreational area', 'https://cdn.skyrisecities.com/sites/default/files/images/articles/2020/05/42264/42264-140124.jpeg', 1, 2, '2018-08-27T07:00:00.000Z', 43.64030802188996, -79.33921027190124);
 
 INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
 VALUES('Stanley Park', 'For Stans Only', 'https://www.planetware.com/photos-large/CDN/canada-vancouver-stanley-park.jpg', 3, 3, '2018-08-27T07:00:00.000Z', 49.3043, -123.1443);
 
+INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
+VALUES('Gastown Clock', 'Steam Clock', 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/gastown-steam-clock-bob-christopher.jpg', 1, 3, '2018-08-27T07:00:00.000Z', 49.28438010592413, -123.10889007953635
+);
+
+INSERT INTO points (title, description, image, created_by, map_id, created_at, latitude, longitude)
+VALUES('Capilano Suspension Bridge', 'Park', 'https://images.dailyhive.com/20180829005357/Capilano-Suspension-Bridge.jpg', 3, 3, '2018-08-27T07:00:00.000Z', 49.34285125334017, -123.11488088005639
+);
